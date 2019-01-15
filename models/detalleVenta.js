5a7892b003acdca23158b4103bcbe2c979cdf347
jeanjpcs18 <41808738+jeanjpcs18@users.noreply.github.com>
2019-01-15 13:10:09 -0500

    modelos

--- models/detalleVenta.js
+++ models/detalleVenta.js
@@ -1,1 +1,43 @@
-detalleVenta.js
+module.exports = function (sequelize, Sequelize){
+	var venta = require('./venta');
+	var Venta = new venta(sequelize, Sequelize);
+
+
+	var DetalleVenta = sequelize.define('detalleVenta', {
+		id: { 
+			autoIncrement: true,
+			primaryKey: tru,
+			type: Sequelize.INTEGER
+
+		},
+		cantidad: {
+			type: Sequelize.DOUBLE
+		},
+		precioUnitario: {
+			type: Sequelize.DOUBLE
+		},
+		precioTotal: {
+			type: Sequelize.DOUBLE
+		},
+		external_id: {
+			type: Sequelize.UUID
+		}
+	}, {
+
+		freezeTableName: true, 
+		timestamps: false
+
+	});
+	DetalleVenta.associate = funtion(models) {
+		models.detalleVenta.hasMany(models.venta,{
+			foreignKey: 'id_persona'
+		});
+	};
+
+	DetalleVenta.belongsTo(Venta, {
+		foreignKey: 'id_venta',
+		constraints: false
+	});
+
+	return DetalleVenta;
+};
---
+++ models/pago.js
@@ -0,0 +1,34 @@
+module.exports =  function (sequelize, Sequelize){
+	var venta = require('./venta');
+	var Venta = new venta(sequelize, Sequelize);
+
+	var Pago = sequelize.define('pago', {
+		id: {
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER
+		},
+		metodoPago: {
+			type: Sequelize.STRING(40)
+		},
+		external_id: {
+			type: Sequelize.UUID
+		}
+	}, {
+
+		freezeTableName: true,
+		timestamps: false
+		
+	});
+	Pago.belongsTo(Venta, {
+		foreignKey: 'id_venta',
+		constraints: false
+	});
+	Pago.associate = function (models){
+		models.pago.hasOne(models.venta, {
+			foreignKey: 'id_pago'
+		});
+	};
+
+	return Pago; 
+};
