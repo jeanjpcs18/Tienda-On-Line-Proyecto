fd8284f1487b80f8b48eaa52d9f7cd727c8715cc
jeanjpcs18 <41808738+jeanjpcs18@users.noreply.github.com>
2019-01-15 16:52:31 -0500

    commit modelos

---
+++ models/rol.js
@@ -0,0 +1,22 @@
+module.exports = function (sequelize, Sequelize) {
+    var Rol = sequelize.define('rol', {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        nombre: {
+            type: Sequelize.STRING(20)
+        }
+    }, {
+        freezeTableName: true, 
+        timestamps: false
+    });
+    
+    Rol.associate = function (models) {
+        models.rol.hasMany(models.persona, {
+            foreignKey: "id_rol"
+        });
+    };
+    return Rol;
+};
