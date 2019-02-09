190329a60757591e91289caa3d126bd905a0ebb2
jeanjpcs18 <41808738+jeanjpcs18@users.noreply.github.com>
2019-02-08 19:30:56 -0500

    Vista con su css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tienda On Line' });
+});
+
+
+router.get('/registra', function(req, res, next) {
+  res.render('registra', { title: 'Registro' });
 });
 
 module.exports = router;
